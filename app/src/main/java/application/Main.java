a3430a6dad6e42ef491ab4e18cff7f75e66b8610
marcoweb <marcocarvalho.web@gmail.com>
2024-04-19 01:19:54 +0000

    InputMismatchException

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,27 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        int[] numeros = new int[5];
+
+        for(int cont = 0; cont < numeros.length; cont++) {
+            try {
+                System.out.println("Digite um Número Inteiro:");
+                Scanner sc = new Scanner(System.in);
+                numeros[cont] = sc.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("Tipo de Dado Inválido");
+                cont--;
+            }
+        }
+
+        System.out.print("Valores Válidos: ");
+        for(int val : numeros) {
+            System.out.print(val + " ");
+        }
     }
 }
 
